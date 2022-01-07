c4274a3cbd3a96cce0f4d8d3896c4808dc5d9634
minniejung <94359917+minniejung@users.noreply.github.com>
2022-01-07 11:13:12 +0100

    Iteration 4

---
+++ config/mongodb.js
@@ -0,0 +1,15 @@
+
+const mongoose = require("mongoose");
+const MONGODB_URI = process.env.MONGO_URI;
+
+console.log(MONGODB_URI);
+mongoose
+  .connect(String(MONGODB_URI))
+  .then((x) => {
+    console.log(
+      `Connected to Mongo! Database name: "${x.connections[0].name}"`
+    );
+  })
+  .catch((err) => {
+    console.error("Error connecting to mongo: ", err);
+  });
